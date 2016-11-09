1646b5c508b84185da08a4de0b6d3a87edae9094
josuerocha <josuerocha@me.com>
2016-11-09 09:39:21 -0200

    NotaDAO Josue 8/11

--- class/DAO/NotaDAO.php
+++ class/DAO/NotaDAO.php
@@ -3,17 +3,17 @@
 
     class NotaDAO extends ACrudDAO{
 
-        function Save($idioma){	
+        function Save($nota){	
 			$situation = TRUE;
 			try{
 				$this->Connect();		
-				if($idioma->getCode()==0){				
-					$query = "insert into tbl_Idioma (descricao_Idioma) values ('{$idioma->getDescricao()}')";
+				if($nota->getCode()==0){				
+					$query = "insert into tbl_Nota (cod_Aluno,mid_Nota,mid_Nota,oral_Nota,ano_Nota,semestre_Nota) values ({$nota->getCodeAluno()},{$nota->getMid()},{$nota->getFinal()},{$nota->getOral()},{$nota->getAno()},{$nota->getSemestre()})";
 					$this->connection->query($query);					
 					$code = $this->conexao->insert_id;
-					$idioma->setCode($code);
+					$nota->setCode($code);
 				}else{	
-					$query = "update tbl_Idioma set descricao_Idioma = '{$idioma->getDescricao()}' where cod_Idioma = {$idioma->getCode()}";
+					$query = "update tbl_Nota set cod_Aluno = {$nota->getCodeAluno()},mid_Nota = {$nota->getMid()},final_Nota = {$nota->getFinal()},oral_Nota = {$nota->getOral()},ano_Nota = {$nota->getAno()},semestre_Nota = {$nota->getSemestre()} where cod_Nota = {$nota->getCode()}";
 					$this->connection->query($query);
 				}
 				$this->Disconnect();
@@ -24,11 +24,11 @@
 			return $situation;
 		}
 
-        function Delete($idioma){
+        function Delete($nota){
             $situation = TRUE;
             try{
                 $this->Connect();	
-                $query = "delete from tbl_Idioma where cod_Idioma = {$idioma->getCode()}";
+                $query = "delete from tbl_Nota where cod_Nota = {$nota->getCode()}";
                 $this->connection->query($query);
                 $this->Disconnect();
             }catch(Exception $ex){
@@ -39,16 +39,16 @@
         }
 
         function List(){
-			$idiomas = array();			
+			$notas = array();			
 			try{
 				$this->Connect();	
-				$query = "select * from tbl_Idioma";
+				$query = "select * from tbl_Nota";
 				$result = $this->connection->query($query);	
 				$this->Disconnect();				
 				while($register = mysqli_fetch_assoc($result)) {
-					$idioma = new Idioma();
-					$idioma->setCode($register['cod_Idioma']);
-                    $idioma->setDescricao($register['descricao_Idioma']);
+					$nota = new Nota();
+					$nota->setCode($register['cod_Nota']);
+                    $nota->setCodeAluno($register['descricao_Nota']);
 					array_push($idiomas, $idioma);
 				}		
 				$result->close();				
@@ -63,3 +63,12 @@
         }
 }
 ?>
+CREATE TABLE `ccaa`.`tbl_Nota` (
+  `cod_Nota` INT NOT NULL,
+  `cod_Aluno` INT NULL,
+  `mid_Nota` FLOAT NULL,
+  `final_Nota` FLOAT NULL,
+  `oral_Nota` FLOAT NULL,
+  `ano_Nota` INT NULL,
+  `semestre_Nota` INT NULL,
+  PRIMARY KEY (`cod_Nota`));
