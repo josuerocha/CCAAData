44eb4510d5a58caf6dd63ecbd987a9b9c7088476
josuerocha <josuerocha@me.com>
2016-11-09 14:43:22 -0200

    MONTAGEMGRADE Josue 9/11

--- class/DAO/TurmaDAO.php
+++ class/DAO/TurmaDAO.php
@@ -7,13 +7,13 @@
 			$situation = TRUE;
 			try{
 				$this->Connect();		
-				if($sala->getCode()==0){				
-					$query = "insert into tbl_Turma (descricao_Sala) values ('{$sala->getDescricao()}')";
+				if($turma->getCode()==0){				
+					$query = "insert into tbl_Turma (tbl_Sala_numero_Sala,tbl_Idioma_cod_Idioma,descricao_Turma,horario_Turma) values ({$turma->getNumSala()},{$turma->getIdioma()},'{$turma->getDescricao()}','{$turma->getHorario()}')";
 					$this->connection->query($query);					
 					$code = $this->connection->insert_id;
 					$sala->setCode($code);
 				}else{	
-					$query = "update tbl_Sala set descricao_Sala = '{$sala->getDescricao()}' where numero_Sala = {$sala->getCode()}";
+					$query = "update tbl_Turma set tbl_Sala_numero_Sala = '{$sala->getNumeroSala()}',tbl_Idioma_cod_Idioma = '{$sala->getIdioma()}',descricao_Turma = '{$turma->getDescricao()}',horario_Turma = '{$turma->getHorario()}' where numero_Sala = {$sala->getCode()}";
 					$this->connection->query($query);
 				}
 				$this->Disconnect();
@@ -24,11 +24,11 @@
 			return $situation;
 		}
 
-        function Delete($sala){
+        function Delete($turma){
             $situation = TRUE;
             try{
                 $this->Connect();	
-                $query = "delete from tbl_Sala where numero_Sala = {$sala->getCode()}";
+                $query = "delete from tbl_Sala where cod_Turma = {$turma->getCode()}";
                 $this->connection->query($query);
                 $this->Disconnect();
             }catch(Exception $ex){
@@ -39,10 +39,10 @@
         }
 
         function List(){
-			$salas = array();			
+			$turmas = array();			
 			try{
 				$this->Connect();	
-				$query = "select * from tbl_Sala";
+				$query = "select * from tbl_Turma";
 				$result = $this->connection->query($query);	
 				$this->Disconnect();				
 				while($register = mysqli_fetch_assoc($result)) {
@@ -63,3 +63,13 @@
         }
 }
 ?>
+CREATE TABLE tbl_Turma (
+  cod_Turma INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  tbl_Sala_numero_Sala INTEGER UNSIGNED NOT NULL,
+  tbl_Idioma_cod_Idioma INTEGER UNSIGNED NOT NULL,
+  descricao_Turma VARCHAR(200) NOT NULL,
+  horario_Turma TIME NOT NULL,
+  PRIMARY KEY(cod_Turma),
+  INDEX tbl_Turma_FKIndex1(tbl_Idioma_cod_Idioma),
+  INDEX tbl_Turma_FKIndex2(tbl_Sala_numero_Sala)
+);
