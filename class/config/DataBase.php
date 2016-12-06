f69c696d4f37ce8ed7bf89358b2e0bfdd3cfe15b
josuerocha <josuerocha@me.com>
2016-12-06 16:05:15 -0200

    Banco com acentos

--- class/config/DataBase.php
+++ class/config/DataBase.php
@@ -6,10 +6,7 @@
 		function __construct(){
 			try{
 				$this->connection = new mysqli("localhost", "root", "", "ccaa");
-				mysql_query("SET NAMES 'utf8'");
-				mysql_query('SET character_set_connection=utf8');
-				mysql_query('SET character_set_client=utf8');
-				mysql_query('SET character_set_results=utf8');
+
 			}catch(Exception $ex){
 				echo $ex->getFile().' : '.$ex->getLine().' : '.$ex->getMessage();
 			}				
--- util/StandardHeader.php
+++ util/StandardHeader.php
@@ -1,4 +1,4 @@
 
-<meta charser="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="icon" href="assets/images/favicon.png" >
 <link rel="favicon" href="pages/assets/images/favicon.png">
