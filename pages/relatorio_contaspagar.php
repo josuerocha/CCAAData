5f7035e15419810389bc86dd834a4e0c2651d07a
josuerocha <josuerocha@me.com>
2016-12-09 13:41:19 -0200

    Relatorio contas a pagar com interface mais bonita

--- pages/relatorio_contaspagar.php
+++ pages/relatorio_contaspagar.php
@@ -5,6 +5,16 @@ require_once("../util/checkSession.php");
 <!DOCTYPE html>
 <html>
 <head>
+<html lang="pt-br">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Relatório de contas a pagar</title>
+<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
+<link rel="stylesheet" href="assets/css/style.css">
+
 <?include "../util/StandardHeader.php" ?>
 <link rel="stylesheet" type="text/css" href="assets/css/datatables/dataTablesCss.css">
 <link rel="stylesheet" type="text/css" href="assets/css/datatables/buttons.dataTables.min.css">
@@ -12,6 +22,39 @@ require_once("../util/checkSession.php");
 </head>
 
 <body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+				<a class="navbar-brand" href="index.html">
+					<img src="assets/images/logo.png" alt="Techro HTML5 template"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right mainNav">
+					<li><a href="home_secretario.php">Home</a></li>
+					<li class="active"><a href="cadastros.php">Cadastros</a></li>
+					<li><a href="cadastro_pessoa.php">Cadastrar Pessoa</a></li>
+					<li><a href="cadastro_contaspagar.php">Contas a Pagar</a></li>
+					<li><a href="cadastro_contasreceber.php">Contas a Receber</a></li>
+          			<li><a href="alterar_senha.php">Alterar Senha</a></li>
+					<li><a href="../util/logout.php">Logout</a></li>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</div>
+	<!-- /.navbar -->
+
+	<header id="head" class="secondary_aluno">
+        <div class="container">
+            <h1 id="texto_modulo">Contas à pagar</h1>
+        </div>
+	</header>
+
+
+<div id="area_relatorio">
 <table id="tabela" class="display nowrap" cellspacing="0" width="100%">
 
 <?PHP
@@ -23,8 +66,6 @@ $contas = $contasControl->ListAll();
 
 $tipoControl = new TipoContaController();
 ?>
-
-
 <thead>
 	<tr> 		
         <th data-type="numeric">Tipo</th>
@@ -64,6 +105,39 @@ $tipoControl = new TipoContaController();
 	</tr>
     </tbody>
 </table>
+</div>
+
+		<div class="footer2_aluno"> <!-- alterar css -->
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="inicio.html">Home</a>
+								<a href="sobre.html">Sobre</a>
+								<a href="cursos.html">Cursos</a>
+          						<a href="estude.html">Estude no CCAA</a>
+          						<a href="unidades.html">Unidades</a>
+								<a href="precos.html">Preços</a>
+          						<a href="convenios.html">Convênios</a>
+          						<a href="contato.html">Contato</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="text-right">
+								Copyright &copy; 2016. Template by<a href="http://Jess&Josh&Nick.com/" rel="develop">Jess&Josh&Nick.com</a>
+							</p>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
 
 </body>
 
